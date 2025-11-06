503628f42389dd3faaea9f0b4bd9dd52fb6a3071
Theadora Yocum <theayocum@mac.com>
2025-11-06 11:33:07 -0500

    Initial Commit

---
+++ Elvis/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Elvis
+//
+//  Created by Thea Yocum on 11/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by Thea Yocum on 11/6/25.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
